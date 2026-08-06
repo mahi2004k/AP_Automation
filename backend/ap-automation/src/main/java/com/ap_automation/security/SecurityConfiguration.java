4aac96908e4e0d298cfbd7c81f8eceb7d81399a8
mahi2004k <mkonnur718@gmail.com>
2026-08-06 19:04:12 +0530

    Added frontend url in Cors

--- backend/ap-automation/src/main/java/com/ap_automation/security/SecurityConfiguration.java
+++ backend/ap-automation/src/main/java/com/ap_automation/security/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package com.ap_automation.security;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -39,6 +40,8 @@ public class SecurityConfiguration {
                 )
 
                 .authorizeHttpRequests(auth -> auth
+
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
 
                         .requestMatchers(
                                 "/api/auth/**",
